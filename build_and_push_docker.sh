db8e1edca8677ecdabb676dc012fc0b1c671816d
Vadim Eisenberg <vadime@il.ibm.com>
2018-03-13 14:37:45 +0200

    added files for envoy as a forward proxy for other pods

--- build_and_push_docker.sh
+++ build_and_push_docker.sh
@@ -24,5 +24,8 @@ echo using $DOCKER_HUB_USER as the user of hub.docker.com
 docker build --tag ${DOCKER_HUB_USER}/envoy-sidecar-forward-proxy:latest envoy_sidecar_forward_proxy
 docker push ${DOCKER_HUB_USER}/envoy-sidecar-forward-proxy:latest
 
+docker build --tag ${DOCKER_HUB_USER}/envoy-forward-proxy:latest envoy_forward_proxy
+docker push ${DOCKER_HUB_USER}/envoy-forward-proxy:latest
+
 docker build --tag ${DOCKER_HUB_USER}/nginx-forward-proxy:latest nginx_forward_proxy
 docker push ${DOCKER_HUB_USER}/nginx-forward-proxy:latest
